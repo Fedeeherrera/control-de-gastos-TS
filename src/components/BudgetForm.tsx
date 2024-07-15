66894f41cb88fa71f76aecbb5ddfc3e8b9e8f060
Fedeeherrera <fedeeherrera2001@gmail.com>
2024-07-15 12:29:05 -0300

    Creacion del input para el presupuesto

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,6 +1,34 @@
+import { useState } from "react"
+
 function BudgetForm() {
+  const [budget, setBudget] = useState(0)
+
+  const handleChange = (e : React.ChangeEvent<HTMLInputElement>)  => {
+    e.preventDefault()
+    setBudget(e.target.valueAsNumber)
+  }
+
   return (
-    <form>BudgetForm</form>
+    <form className="space-y-5">
+      <div className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-4xl text-blue-600 font-bold text-center"
+        >
+          Definir Presupuesto
+        </label>
+        <input
+          type="number"
+          className="w-full bg-white border border-gray-200 p-2 "
+          placeholder="Define tu presupuesto"
+          name="budget"
+          id="budget"
+          value={budget}
+          onChange={handleChange}
+        />
+      </div>
+      <input type="submit" value='Definir Presupuesto' className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black" />
+    </form>
   )
 }
 
