bb3a6830f2faecc21eeac8aa9778af4be6a860fb
Fedeeherrera <fedeeherrera2001@gmail.com>
2024-08-23 13:17:38 -0300

    creacion de handleSubmit en ExpenseForm

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -32,8 +32,15 @@ function ExpenseForm() {
       date: value,
     })
   }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log("Enviando")
+  }
+
+
   return (
-    <form className="space-y-5">
+    <form className="space-y-5" onSubmit={handleSubmit}>
       <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">
         Nuevo Gasto
       </legend>
